2861177978e7d51d4a397d451c5b070900d0d164
shenaishiren <gjw.jesus@qq.com>
2018-08-21 09:47:18 +0800

    add acc

--- src/SRC_PA/parquet_kernel.f90
+++ src/SRC_PA/parquet_kernel.f90
@@ -3,7 +3,7 @@ module Parquet_kernel
   ! Purpose
   ! ========
   !   determine the reducible vertex function in particle-hole and particle-particle channels
-  ! 
+  !
   use mpi_mod
   use global_parameter
   use math_mod
@@ -12,25 +12,25 @@ module Parquet_kernel
 contains
   !-------------------------------------------------------------------------------------------
   subroutine reducible_vertex(ite)
-     
+
     integer, intent(in) :: ite
 
     ! ... local vars ...
     integer     :: i, j, k, idx, idx1
     complex(dp) :: dummy, G1
     character(len=30) :: FLE, str
-    
+
     complex(dp), allocatable :: dummy3D_1(:, :), dummy3D_2(:, :), dummy3D_3(:, :), dummy3D_4(:, :)
     type(Indxmap) :: ComIdx1, ComIdx2
-    
+
     if (.NOT. allocated(dummy3D_1)) allocate(dummy3D_1(Nt, Nt))
     if (.NOT. allocated(dummy3D_2)) allocate(dummy3D_2(Nt, Nt))
     if (.NOT. allocated(dummy3D_3)) allocate(dummy3D_3(Nt, Nt))
     if (.NOT. allocated(dummy3D_4)) allocate(dummy3D_4(Nt, Nt))
-    
+
     do k = 1, Nb
        idx = id*Nb + k
-      
+
        ! start to calculate the reducible vertex function in particle-hole channel
        G1        = Zero_c
        dummy3D_3 = Zero_c
@@ -41,20 +41,20 @@ contains
        do i = 1, Nt
           call index_operation(Index_fermionic(i), Index_bosonic(idx), FaddB, ComIdx1) ! k+q
           idx1 = list_index(ComIdx1, Fermionic)
-          
+
           if (ComIdx1%iw > Nf .or. ComIdx1%iw < 1) then
              ! use the non-interacting Green's function when k+q is outside of the box
              dummy = One/(xi*Pi/beta*(Two*(ComIdx1%iw-Nf/2-1) + One) + mu - Ek(ComIdx1%ix, ComIdx1%iy))
           else
-             dummy = Gkw(idx1) 
+             dummy = Gkw(idx1)
           end if
           do j = 1, Nt
              dummy3D_1(i, j) = F_d(i, j, k)*Gkw(i)*dummy/beta/Nc
-             dummy3D_2(i, j) = F_m(i, j, k)*Gkw(i)*dummy/beta/Nc 
+             dummy3D_2(i, j) = F_m(i, j, k)*Gkw(i)*dummy/beta/Nc
           end do
           G1 = G1 + xU*Gkw(i)*dummy*xU/beta/Nc
        end do
-       
+
        call ZGEMM('N', 'N', Nt, Nt, Nt, Half_c, G_d(1:Nt, 1:Nt, k), Nt, dummy3D_1(1:Nt, 1:Nt), Nt, Zero_c, dummy3D_3, Nt)
        call ZGEMM('N', 'N', Nt, Nt, Nt, Half_c, G_m(1:Nt, 1:Nt, k), Nt, dummy3D_2(1:Nt, 1:Nt), Nt, Zero_c, dummy3D_4, Nt)
 
@@ -77,7 +77,8 @@ contains
 
        call ZGEMM('N', 'N', Nt, Nt, Nt, Half_c, dummy3D_1(1:Nt, 1:Nt), Nt, G_d(1:Nt, 1:Nt, k), Nt, One_c, dummy3D_3, Nt)
        call ZGEMM('N', 'N', Nt, Nt, Nt, Half_c, dummy3D_2(1:Nt, 1:Nt), Nt, G_m(1:Nt, 1:Nt, k), Nt, One_c, dummy3D_4, Nt)
-       
+
+       !$acc parallel loop
        do i = 1, Nt
           do j = 1, Nt
              if (ite == 1) then
@@ -89,8 +90,8 @@ contains
              end if
           end do
        end do
-    
-       ! --- singlet (s) and triplet (t) channel --- 
+
+       ! --- singlet (s) and triplet (t) channel ---
        G1        = Zero_c
        dummy3D_3 = Zero_c
        dummy3D_4 = Zero_c
@@ -147,7 +148,7 @@ contains
              end if
           end do
        end do
-       
+
     end do
 
     write(str, '(I0.3,a,I0.3)') ite, '-', id
@@ -170,7 +171,7 @@ contains
     if (allocated(dummy3D_4)) deallocate(dummy3D_4)
 
     !!! G_d, G_m, G_s, G_t are now updated, which temporarily store the reducible vertex functions.
-       
+
   end subroutine reducible_vertex
 
   ! ----------------------------------------------------------------------------------------------------------------
@@ -178,8 +179,8 @@ contains
     !
     ! Purpose
     ! =======
-    !   From the exact reducible vertex function calculated in routine "reducible_Vertex" determine  
-    ! the kernel functions by scanning the edge of it. 
+    !   From the exact reducible vertex function calculated in routine "reducible_Vertex" determine
+    ! the kernel functions by scanning the edge of it.
     !
     integer, intent(in) :: ite
 
@@ -198,7 +199,7 @@ contains
     if (.NOT. allocated(K2_s2)) allocate(K2_s2(Nc, Nt, Nt/2))
     if (.NOT. allocated(K2_t2)) allocate(K2_t2(Nc, Nt, Nt/2))
 
-    ! assign trivial initial values to kernel functions 
+    ! assign trivial initial values to kernel functions
     if (ite == 1) then
        K2_d1 = Zero_c
        k2_d2 = Zero_c
@@ -246,7 +247,7 @@ contains
           case (1)
              K2_d1(1:Nt, 1:Nc, 1:Nb) = dummy1(1:Nt, 1:Nc, 1:Nb)
              K2_d2(1:Nc, 1:Nt, 1:Nb) = dummy2(1:Nc, 1:Nt, 1:Nb)
-          case (2) 
+          case (2)
              K2_m1(1:Nt, 1:Nc, 1:Nb) = dummy1(1:Nt, 1:Nc, 1:Nb)
              K2_m2(1:Nc, 1:Nt, 1:Nb) = dummy2(1:Nc, 1:Nt, 1:Nb)
           case (3)
@@ -294,34 +295,34 @@ contains
        FLE = 'K2-'//trim(str)//'.dat'
        open(unit = 1, file = FLE, status = 'unknown')
        do i = 1, Nt
-          do ic = 1, Nc 
+          do ic = 1, Nc
              do k = 1, Nt/2
                 write(1, '(3i4, 16f10.5)') i, ic, k, K2_d1(i,ic,k), K2_d2(ic,i,k), K2_m1(i,ic,k), K2_m2(ic,i,k), &
                      K2_s1(i,ic,k), K2_s2(ic,i,k), K2_t1(i,ic,k), K2_t2(ic,i,k)
              end do
-             write(1, *) 
+             write(1, *)
           end do
        end do
        close(1)
     end if
-    
+
     if (allocated(dummy1)) deallocate(dummy1)
     if (allocated(dummy2)) deallocate(dummy2)
 
   end subroutine get_kernel_function
- 
+
   !-------------------------------------------------------------------------------------------------
   complex(dp) function kernel(channel, k1, k2, q)
-    
+
     ! character(len=1), intent(in) :: channel
     integer, intent(in) :: channel
     type(Indxmap),    intent(in) :: k1, k2, q
-  
+
     ! ... local vars ...
-    integer       :: ic, jc, qc, iw, jw, qw, idx, idx1 
+    integer       :: ic, jc, qc, iw, jw, qw, idx, idx1
     type(Indxmap) :: k1_, k2_, q_
     complex(dp)   :: background
-    
+
     if (q%iw > 0) then
        k1_ = k1
        k2_ = k2
@@ -340,14 +341,14 @@ contains
     qw = q_%iw
 
     idx  = ((k1_%ix-1)*Ny+k1_%iy)*Nf
-    idx1 = ((k1_%ix-1)*Ny+k1_%iy-1)*Nf+1 
+    idx1 = ((k1_%ix-1)*Ny+k1_%iy-1)*Nf+1
 
     kernel = Zero
     if (qw > Nf/2) return
 
     select case (channel)
     case (CHANNEL_D)
-    
+
           background = K2_d1(idx, jc, list_index(q_, Bosonic))
 
        if (iw > Nf .or. iw < 1) then  ! k1 is out of range
@@ -362,7 +363,7 @@ contains
           else
              Kernel = K2_d1(list_index(k1_, Fermionic), jc, list_index(q_, Bosonic)) + K2_d2(ic, list_index(k2_, Fermionic), list_index(q_, Bosonic)) - background
           end if
-       end if       
+       end if
     case (CHANNEL_M)
           background = K2_m1(idx, jc, list_index(q_, Bosonic))
        if (iw > Nf .or. iw < 1) then  ! k1 is out of range
@@ -409,11 +410,11 @@ contains
           end if
        end if
     end select
-     
+
     if (q%iw < 1)  Kernel = conjg(Kernel)
 
-  end function kernel 
-  
+  end function kernel
 
-  
+
+
 end module Parquet_kernel
